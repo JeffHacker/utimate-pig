06471a06c1c6b9a7e3daf0f40c89146f820d0d17
Jeff Hacker <reachjkh@yahoo.com>
2015-07-20 22:53:56 -0400

    unknown changes

---
+++ pig_basic.py
@@ -0,0 +1,35 @@
+import random
+
+
+
+round_count = 0
+total_point_count 
+total_point_count = []
+die = random.choice([1, 2, 3, 4, 5, 6])
+
+total_point_count.append(die)
+
+round_count = die
+
+if die == 1:
+    round_count = 0
+else:
+
+    print(round_count)
+    print(total_point_count)
+
+
+def die_roll():
+
+    return die
+
+def add_points():
+    pass
+
+def total_points():
+    player.total_point_count += player.round_count
+    round_count = 0
+
+
+
+
